0f26d8af74c9ada30caf9a0b5d49b77c137a319c
Beingpax <prakashjoshipaxtek@gmail.com>
2022-05-19 22:52:13 +0545

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,26 +11,4 @@ module.exports = {
   siteMetadata: {
     title: `🌱Digital Garden`,
   },
-  plugins: [
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
-        footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
-        gfm: true,
-        // Plugins configs
-        plugins: [{
-              resolve: `gatsby-remark-link-unfurl`,
-              options: {
-                processedUrlsFile: `${__dirname}/link-cache/cache.json`,
-              },
-            }],
-      },
-    },
-  ],
 }
